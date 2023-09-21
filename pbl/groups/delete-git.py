7cff4606b1b8ca27b879bb575cdd728b35052947
Yabushita111 <anedtost@yahoo.co.jp>
2023-09-21 17:16:45 +0900

    add all groups

--- pbl/groups/delete-git.py
+++ pbl/groups/delete-git.py
@@ -1,7 +1,5 @@
 import subprocess
 
 for i in range(1,15):
-    rm = 'rm -rf pblb2023g'+str(i).zfill(2)+'/.git'
-    remove = 'git remote remove pblb2023g'+str(i).zfill(2)+'/origin'
+    rm = 'git rm -rf --cached pblb2023g'+str(i).zfill(2)
     subprocess.run(rm,shell=True)
-    subprocess.run(remove,shell=True)
